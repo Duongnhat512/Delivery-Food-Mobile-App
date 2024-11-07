3df0c20311a1b11906135045cc2377daad85a522
Bao <Bao@gmail.com>
2024-11-08 00:16:35 +0700

    cap nhat file configFirebase

--- delivery-food/FirebaseConfig.js
+++ delivery-food/FirebaseConfig.js
@@ -4,14 +4,23 @@ import { getFirestore } from "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID } from '@env';
 
+// Log environment variables to debug
+console.log("FIREBASE_API_KEY:", FIREBASE_API_KEY);
+console.log("FIREBASE_AUTH_DOMAIN:", FIREBASE_AUTH_DOMAIN);
+console.log("FIREBASE_PROJECT_ID:", FIREBASE_PROJECT_ID);
+console.log("FIREBASE_STORAGE_BUCKET:", FIREBASE_STORAGE_BUCKET);
+console.log("FIREBASE_MESSAGING_SENDER_ID:", FIREBASE_MESSAGING_SENDER_ID);
+console.log("FIREBASE_APP_ID:", FIREBASE_APP_ID);
+console.log("FIREBASE_MEASUREMENT_ID:", FIREBASE_MEASUREMENT_ID);
+
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: FIREBASE_API_KEY,
+    authDomain: FIREBASE_AUTH_DOMAIN,
+    projectId: FIREBASE_PROJECT_ID,
+    storageBucket: FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+    appId: FIREBASE_APP_ID,
+    measurementId: FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
