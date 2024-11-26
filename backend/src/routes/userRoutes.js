6dce4f1ae844a5f7cdb770e1c88f57116241dee9
Bao <Bao@gmail.com>
2024-11-26 19:00:59 +0700

    them add payment

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -6,5 +6,6 @@ router.post('/sign_up', userController.signUp);
 router.get('/get_user', userController.getUser);
 router.post('/update_address', userController.updateAddress);
 router.put('/update_user', userController.updateUser);
+router.post('/addPayment', userController.addPayment);
 
 module.exports = router;
