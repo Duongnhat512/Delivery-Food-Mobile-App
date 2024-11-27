74fd4aa1a343a706faed46c16513499b67520657
Bao <Bao@gmail.com>
2024-11-27 12:55:40 +0700

    cap nhat gio hang

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -5,5 +5,6 @@ const orderController = require('../controllers/orderController');
 router.post('/add_order', orderController.addOrder);
 router.get('/get_orders', orderController.getOrder);
 router.put('/update_status/:order_id/:item_id', orderController.updateStatusOrderDetail);
+router.put('/update_quantity_and_total/:order_id/:item_id', orderController.updateQuantityAndTotalOrderDetail);
 
 module.exports = router;
