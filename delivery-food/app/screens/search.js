7d1749772c007323d95fe658736c6c06fde17e58
Duongnhat512 <duongnhat512@gmail.com>
2024-11-27 15:18:33 +0700

    thay doi thanh tim kiem

---
+++ delivery-food/app/screens/search.js
@@ -0,0 +1,13 @@
+import { Text, View, StyleSheet } from 'react-native'
+import React, { Component } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Seach = () => {
+    return (
+        <SafeAreaView>
+            
+        </SafeAreaView>
+    )
+}
+
+export default Seach
---
+++ delivery-food/app/screens/searchResults.js
@@ -0,0 +1,13 @@
+import { Text, View, StyleSheet } from 'react-native'
+import React, { Component } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const SearchResults = () => {
+    return (
+        <SafeAreaView>
+
+        </SafeAreaView>
+    )
+}
+
+export default SearchResults
